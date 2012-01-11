4937981467c4e14b2e2db1ae03bd1ecbe55609dc
Revath S Kumar <revath@mobme.in>
2012-01-11 16:47:24 +0530

    added functionality login and send sms

--- lib/way2sms.rb
+++ lib/way2sms.rb
@@ -1,13 +1,63 @@
+require "net/http"
+require "net/https"
+require "uri"
+
 class Way2sms
   attr_accessor :username, :password
+
+  URL = 'http://site6.way2sms.com'
  
-  def initialize(username, password)
+  def initialize(username = "", password = "")
     @username = username
     @password = password
-    
+    @uri = URI.parse URL
+    @cookie = nil
+    @referer = URL
+    @http = Net::HTTP.new(@uri.host,@uri.port)
+  end
+
+  def set_header(cookie=nil,referer=nil)
+    {"Cookie" => cookie , "Referer" => referer ,"Content-Type" => "application/x-www-form-urlencoded",
+      "User-Agent" => "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20091020 Ubuntu/9.10 (karmic) Firefox/3.5.3 GTB7.0" }
   end
 
   def login
+    data = 'username='+@username+'&password='+@password
+    headers = set_header @cookie, @referer
+    response = @http.post("/Login1.action",data,headers.delete_if {|i,j| j.nil? })
+    case response.code
+      when  /3\d{2}/
+        if response['location'].include?("Main.action")
+          @cookie ||= response['set-cookie']
+          @referer ||= response['referer']
+          return {:success => true,:message => "Login successfully"}
+        end
+        return {:success => false,:message => "Login failed"}
+      else
+        return "Http Error"
+    end
+  end
 
+  def send_sms msisdn,message
+    headers = set_header @cookie, @referer
+    data = "MobNo=#{msisdn}&textArea=#{message}&HiddenAction=instantsms&login=&pass=&Action=abfghst5654g"
+    return @http.post("/quicksms.action?custid=\"+custid+\"&sponserid=\"+sponserid+\"",data,headers.delete_if {|i,j| j.nil? })
+  end
+
+  def send msisdn,message
+    response = send_sms msisdn,message
+    case response.code
+      when  /2\d{2}/
+        return {:success => true,:message => "Send successfully"}
+      else
+        return {:success => false,:message => "Sending failed"}
+    end
+  end
+
+  def send_to_group msisdns, message
+    msisdns = msisdns.split(';')
+    msisdns.each | msisdn | do
+      response = send_sms msisdn,message
+    end
   end
 end
--- spec/way2sms_spec.rb
+++ spec/way2sms_spec.rb
@@ -4,36 +4,41 @@ require 'way2sms'
 
 describe Way2sms, "send SMS via way2sms" do
 
-  let(:sms) { way2sms.new }
+  let(:sms) { Way2sms.new '9995436867','123456'}
+
+  it "should respond to login, set_header, send_sms" do
+    sms.should respond_to :login
+    sms.should respond_to :send_sms
+    sms.should respond_to :set_header
+  end
 
   it "should fail to login attempt" do
-    sms.username = '9995436867'
     sms.password = '123456'
-    sms.login
+    sms.login.should == {:success => false,:message => "Login failed"}
   end
 
-  it "should login successfully" do
-
-
-  end
+  # it "should login successfully" do
+  #   sms.login.should == {:success => true,:message => "Login successfully"}
+  # end
 
   it "should accept only two params" do 
 
   end
 
-  it "should accept either a group of numbers or a single msisdn" do 
+  it "should accept either a group of numbers or a single msisdn" do
 
 
   end
 
 
   it "should send SMS to individual" do
-
+    sms.login
+    sms.send_sms('9995436867', 'Testing Ruby!!').should == {:success => true,:message => "Send successfully"}
   end
 
   it "should accept send SMS to group" do
-
-
+    sms.login
+    sms.send_to_group('9995436867;9037864203;9037107542', 'Testing Ruby!!').should == {:success => true,:message => "Send successfully"}
   end
 
   it "successfully logged out" do 
